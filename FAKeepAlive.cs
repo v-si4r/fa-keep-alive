0000c0f67fba4b02bd4772dea2c985bab4a65cea
Vadym Sichkar <vadym.sichkar@gi-de.com>
2023-02-18 21:23:09 +0100

    array variable is added

--- FAKeepAlive.cs
+++ FAKeepAlive.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace fa_keep_alive_test
@@ -10,15 +13,26 @@ namespace fa_keep_alive_test
     public class FAKeepAlive
     {
         [FunctionName("fa_keep_alive")]
-        public void Run([TimerTrigger("%ScheduleExpression%")] TimerInfo myTimer, ILogger log)
+        public async Task RunAsync([TimerTrigger("%ScheduleExpression%")] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            Task.Run(async () =>
+            var websites = new List<Website>();
+
+            var config = new ConfigurationBuilder()
+               .SetBasePath(context.FunctionAppDirectory)
+               .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            int i = 0;
+            string url = config.GetSection($"Websites:Urls:{i}").Value;
+
+            while (url != null)
             {
                 using (var client = new HttpClient())
                 {
-                    var response = await client.GetAsync("https://foo.com");
+                    var response = await client.GetAsync(url);
 
                     if (response != null && response.IsSuccessStatusCode)
                     {
@@ -33,7 +47,15 @@ namespace fa_keep_alive_test
                         log.LogError(response?.ToString());
                     }
                 }
-            }).Wait();
+
+                url = config.GetSection($"Websites:Urls:{++i}").Value;
+            }       
+
+        }
+
+        private struct Website
+        {
+            string Url { get; set; }
         }
     }
 }
